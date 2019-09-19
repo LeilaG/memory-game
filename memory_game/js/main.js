fc7fa04b0c59dd1fff4d24ca8b346e66aad5ebf6
Leila Germian <lgeramian@gmail.com>
2019-09-19 19:57:18 -0400

    flipping cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,20 @@
 // console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
 
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
