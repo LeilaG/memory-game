582ae3df5b105ba137cf8616c4c4237b09495e89
Leila Germian <lgeramian@gmail.com>
2019-09-22 11:49:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,23 @@
-// console.log("Up and running!");
-
-// var cardOne = "queen";
-// var cardTwo = "king";
-// var cardThree = "queen";
-// var cardFour = "king";
-
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardTwo);
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-} else {
-    alert("Sorry, try again.");
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
+    }
 }
+
+function flipCard(cardId) {
+    var currentCard = cards[cardId]
+    cardsInPlay.push(currentCard)
+
+    console.log("User flipped " + currentCard);
+    // console.log("cardsInPlay: " + cardsInPlay)
+
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
