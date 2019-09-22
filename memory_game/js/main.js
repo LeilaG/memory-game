8d16ff5b1fa52a2688df557f177a4c3a38d719d3
Leila Germian <lgeramian@gmail.com>
2019-09-22 11:06:21 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -15,6 +15,12 @@ var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
 
-var cardTwo = cards[2];
+var cardTwo = cards[1];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
